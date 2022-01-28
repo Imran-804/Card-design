35cb7078c4df3fe0fb1d512b8e9d7343263787bc
Imran-804 <imranbd804@gmail.com>
2022-01-28 23:51:00 +0600

    card-design-react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Card from './componates/Design';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Card
+        h1 = "Md Emran Hossain"  p = "He is good boy of the room in a there in good of a every come in a any dear" btn = "Submit"
+      />
+      <Card 
+       h1 = "Md Rakib Hossain" p= "He is bad boy in the area of a good mamy come of a dear in lose in genaration " btn = "Click Me"
+      />
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
