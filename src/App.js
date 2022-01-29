cb605ce2b946e16c38cba2d0a5d4fc77ad636296
Imran-804 <imranbd804@gmail.com>
2022-01-29 11:45:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import './App.css';
-import Card from './componates/Design';
+
 
 function App() {
+  let Products = [
+    {names:"photoshop", Prise:"$90.00"},
+    {names:"ilastorr", Prise: "$33.00"}
+  ];
   return (
     <div className="App">
       <header className="App-header">
-      <Card
-        h1 = "Md Emran Hossain"  p = "He is good boy of the room in a there in good of a every come in a any dear" btn = "Submit"
-      />
-      <Card 
-       h1 = "Md Rakib Hossain" p= "He is bad boy in the area of a good mamy come of a dear in lose in genaration " btn = "Click Me"
-      />
+    {/* arrey indes */}
+    {/* <Product name={Products[0].names} Prise={Products[1].Prise}></Product>
+      <Product name={Products[1].names} Prise={Products[1].Prise} ></Product> */}
+
+    <Product Product={Products[0]}></Product>
+    <Product Product={Products[1]}></Product>
       </header>
     </div>
   );
 }
 
+
+function Product(props) {
+  const ProductStyle ={
+    height: "400px",
+    width: '400px',
+    border: '2px solid gray',
+    backgroundColor: 'lightgray',
+    borderRadius: '20px'
+  }
+  const btnStyle ={
+    padding: '10px',
+    fontSize: '20px'
+  }
+  return (
+    <div className="Card" style={ProductStyle}>
+      {/* <h1>{props.names}</h1> */}
+      {/* <p>{props.Prise}</p> */}
+      <h1>{props.Product.names}</h1>
+      <p>{props.Product.Prise}</p>
+      <button style={btnStyle}>buy now</button>
+    </div>
+  )
+  
+}
 
 export default App;
